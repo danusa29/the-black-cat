fb94d5320f04b1b609694d8e7c24c064f99ec6f1
Daniel Wist <dan@Daniels-MacBook-Pro-2.local>
2024-05-24 13:39:39 -0400

    first

--- app/page.js
+++ app/page.js
@@ -10,26 +10,24 @@ export default async function Home() {
         <main className="max-w-7xl mx-auto my-auto pt-10 pb-10 pl-4 pr-4">
             <h1 class="pb-14">The Black Cat</h1>
             {menu.map(({ name, cocktails }) =>
-                // Check if the category has any cocktails
                 cocktails.length ? (
                     <section class="pb-9" key={name}>
                         <h2 class="pb-5">// {name}</h2>
                         {cocktails.map(({ title, top_tier, notes, glass }) => (
                             <div
                                 className={
-                                    // Add padding bottom if the cocktail has notes
                                     notes ? "[&:not(:last-child)]:pb-5" : ""
                                 }
                                 key={title}
                             >
                                 <h3>
                                     {title}
-                                    {/* Add asterisks if the cocktail is top-tier */}
+
                                     {top_tier ? " **" : null}
                                 </h3>
-                                {/* Render notes if available */}
+
                                 {notes && <p class="mt-1">{notes}</p>}
-                                {/* Render glass information if available */}
+
                                 {glass && (
                                     <p class="small mt-1 italic">
                                         {`Served in a ${glass}.`}
@@ -38,8 +36,7 @@ export default async function Home() {
                             </div>
                         ))}
                     </section>
-                ) : // If the category has no cocktails, render nothing
-                null
+                ) : null
             )}
         </main>
     );
